78ffc2d88afbed3a911d1dce78cd85f2ae0ea0c7
MatgamFR <80062640+MatgamFR@users.noreply.github.com>
2025-03-12 15:39:32 +0100

    Quelques modifications

--- utilisateurs/forms.py
+++ utilisateurs/forms.py
@@ -6,6 +6,18 @@ from datetime import date, timedelta
 class InscriptionForm(UserCreationForm):
     date_de_naissance = forms.DateField(widget=forms.DateInput(attrs={'type': 'date'}))
     genre = forms.ChoiceField(choices=Utilisateur.GENRE_CHOIX, widget=forms.Select())
+    
+    password2 = forms.CharField(
+        label="Confirmation du mot de passe",
+        widget=forms.PasswordInput,
+        help_text=""  # On supprime le message par défaut
+    )
+
+    password1 = forms.CharField(
+        label="Mot de passe",
+        widget=forms.PasswordInput,
+        help_text=""  # Enlève tous les messages d'aide par défaut
+    )
 
     class Meta:
         model = Utilisateur
@@ -14,19 +26,17 @@ class InscriptionForm(UserCreationForm):
     def clean_date_de_naissance(self):
         date_naissance = self.cleaned_data.get("date_de_naissance")
 
-        # Vérifier si la date est dans le futur
         if date_naissance > date.today():
             raise forms.ValidationError("La date de naissance ne peut pas être dans le futur.")
 
-        # Vérifier si l'utilisateur a au moins 13 ans
         age_minimum = date.today() - timedelta(days=13*365)
         if date_naissance > age_minimum:
             raise forms.ValidationError("Vous devez avoir au moins 13 ans pour vous inscrire.")
 
         return date_naissance
 
-def clean_email(self):
-    email = self.cleaned_data.get("email")
-    if Utilisateur.objects.filter(email=email).exists():
-        raise forms.ValidationError("Cet email est déjà utilisé. Veuillez en choisir un autre.")
-    return email
+    def clean_email(self):
+        email = self.cleaned_data.get("email")
+        if Utilisateur.objects.filter(email=email).exists():
+            raise forms.ValidationError("Cet email est déjà utilisé. Veuillez en choisir un autre.")
+        return email
