938d82cd69727198dd729a9e75c6dbcca04569ee
chrmil <130459909+chrmil@users.noreply.github.com>
2025-03-29 17:24:13 +0100

    Update urls.py

--- utilisateurs/urls.py
+++ utilisateurs/urls.py
@@ -15,7 +15,6 @@ urlpatterns = [
     path('traiteur/', traiteur, name='traiteur'),
     path('institut/', institut, name='institut'),
     path('terrasse/', terrasse, name='terrasse'),
-    path('evenements/', evenements, name='evenements'),
     path('histoire/', histoire, name='histoire'),
     path('verifier_email/<int:user_id>/', verifier_email, name='verifier_email'),  # Ajout du paramètre user_id
     path("reservation/", reservation_page, name="reservation"),
