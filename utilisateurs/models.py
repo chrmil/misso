13939f356b292b568d0999552ca30f2f35dc1514
chrmil <cmillet123@outlook.fr>
2025-04-04 11:30:49 +0200

    update page

--- utilisateurs/models.py
+++ utilisateurs/models.py
@@ -44,7 +44,7 @@ class EmailVerification(models.Model):
     is_verified = models.BooleanField(default=False)  # Ajoute ce champ
 
 
-class Page(models.Models):
+class Page(models.Model):
     nom = models.CharField(max_length=100, unique=True)
     emplacement = models.URLField(max_length=500, blank=True, null=True)
     titre = models.TextField(max_length=500, blank=True, null=True)
@@ -54,7 +54,7 @@ class Page(models.Models):
     def __str__(self):
         return self.nom
 
-class Section(models.Models):
+class Section(models.Model):
     nom = models.CharField(max_length=100, unique=True)
     page = models.ForeignKey(Page, on_delete=models.CASCADE)
     titre = models.TextField(max_length=500, blank=True, null=True)
@@ -63,7 +63,7 @@ class Section(models.Models):
     def __str__(self):
         return self.nom
 
-class Paragraphe(models.Models):
+class Paragraphe(models.Model):
     nom = models.CharField(max_length=100, unique=True)
     section = models.ForeignKey(Section, on_delete=models.CASCADE)
     titre = models.TextField(max_length=500, blank=True, null=True)
@@ -72,7 +72,7 @@ class Paragraphe(models.Models):
     def __str__(self):
         return self.nom
 
-class Image(models.Models):
+class Image(models.Model):
     nom = models.CharField(max_length=100, unique=True)
     section = models.ForeignKey(Section, on_delete=models.CASCADE)
     image_path = models.CharField(max_length=300, blank=True, null=True) 
