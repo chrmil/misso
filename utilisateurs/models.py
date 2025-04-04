5dab05b696a131f265c34eb63f0c036014a63b33
chrmil <cmillet123@outlook.fr>
2025-04-04 11:34:32 +0200

    move page models

--- utilisateurs/models.py
+++ utilisateurs/models.py
@@ -43,38 +43,3 @@ class EmailVerification(models.Model):
     created_at = models.DateTimeField(auto_now_add=True)
     is_verified = models.BooleanField(default=False)  # Ajoute ce champ
 
-
-class Page(models.Model):
-    nom = models.CharField(max_length=100, unique=True)
-    emplacement = models.URLField(max_length=500, blank=True, null=True)
-    titre = models.TextField(max_length=500, blank=True, null=True)
-    sous_titre = models.TextField(max_length=500, blank=True, null=True)
-    image_path = models.CharField(max_length=255, blank=True, null=True) 
-    
-    def __str__(self):
-        return self.nom
-
-class Section(models.Model):
-    nom = models.CharField(max_length=100, unique=True)
-    page = models.ForeignKey(Page, on_delete=models.CASCADE)
-    titre = models.TextField(max_length=500, blank=True, null=True)
-    sous_titre = models.TextField(max_length=500, blank=True, null=True)
-    visible = models.BooleanField(default=True) 
-    def __str__(self):
-        return self.nom
-
-class Paragraphe(models.Model):
-    nom = models.CharField(max_length=100, unique=True)
-    section = models.ForeignKey(Section, on_delete=models.CASCADE)
-    titre = models.TextField(max_length=500, blank=True, null=True)
-    sous_titre = models.TextField(max_length=500, blank=True, null=True)
-    texte = models.TextField(blank=True, null=True)
-    def __str__(self):
-        return self.nom
-
-class Image(models.Model):
-    nom = models.CharField(max_length=100, unique=True)
-    section = models.ForeignKey(Section, on_delete=models.CASCADE)
-    image_path = models.CharField(max_length=300, blank=True, null=True) 
-    def __str__(self):
-        return self.nom
