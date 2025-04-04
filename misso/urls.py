a0ec817f44389f127c30ad0d89cb5b9437de867e
chrmil <cmillet123@outlook.fr>
2025-04-04 11:36:16 +0200

    update

--- misso/urls.py
+++ misso/urls.py
@@ -5,7 +5,8 @@ from django.shortcuts import redirect
 urlpatterns = [
     path('restaurants/', include('menu.urls')),
     path('admin/', admin.site.urls),
-    path('', include('utilisateurs.urls')),
+    path('', include('page.urls')),
+    path('user/', include('utilisateurs.urls')),
     path('evenements/',include('evenements.urls')),
     path('', lambda request: redirect('accueil/', permanent=False)),
     path("objets_connectes/", include("objets_connectes.urls")),
